86c57c505983f56feeee1c1d77b72a3219b43bcd
Vipul Daripkar <vipuldaripkar@gmail.com>
2020-11-22 18:13:59 +0530

    Added a spinner to training screen

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -11,6 +11,8 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatListModule } from '@angular/material/list';
 import { MatTabsModule } from '@angular/material/tabs';
 import { MatCardModule } from '@angular/material/card';
+import { MatSelectModule } from '@angular/material/select';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
 @NgModule({
   imports: [
@@ -26,6 +28,8 @@ import { MatCardModule } from '@angular/material/card';
     MatListModule,
     MatTabsModule,
     MatCardModule,
+    MatSelectModule,
+    MatProgressSpinnerModule,
   ],
   exports: [
     MatButtonModule,
@@ -40,6 +44,8 @@ import { MatCardModule } from '@angular/material/card';
     MatListModule,
     MatTabsModule,
     MatCardModule,
+    MatSelectModule,
+    MatProgressSpinnerModule,
   ],
 })
 export class MaterialModule {}
