bf083f19353dcd54e3740602c7cb60099464db1a
Vipul Daripkar <vipuldaripkar@gmail.com>
2020-11-22 16:25:27 +0530

    Started work on training. added card to new training template

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -9,6 +9,8 @@ import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatListModule } from '@angular/material/list';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatCardModule } from '@angular/material/card';
 
 @NgModule({
   imports: [
@@ -22,6 +24,8 @@ import { MatListModule } from '@angular/material/list';
     MatSidenavModule,
     MatToolbarModule,
     MatListModule,
+    MatTabsModule,
+    MatCardModule,
   ],
   exports: [
     MatButtonModule,
@@ -34,6 +38,8 @@ import { MatListModule } from '@angular/material/list';
     MatSidenavModule,
     MatToolbarModule,
     MatListModule,
+    MatTabsModule,
+    MatCardModule,
   ],
 })
 export class MaterialModule {}
